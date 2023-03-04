7304a0e1a9d3276d11071aacb38159510f28a733
Md. Tarik Aziz <69706577+aziztarik238@users.noreply.github.com>
2023-03-04 21:17:24 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import "./App.css";
+import React from "react";
+import PDFFile from "./components/PDFFile";
+import { PDFDownloadLink } from "@react-pdf/renderer";
+// import ImagePicker from "./components/ImagePicker";
+
+const App = () => {
+  return (
+    <div className="App">
+      <PDFDownloadLink document={<PDFFile />} filename="FORM">
+        {({ loading }) =>
+          loading ? (
+            <button>Loading Document...</button>
+          ) : (
+            <button>Download</button>
+          )
+        }
+      </PDFDownloadLink>
+      {/* <PDFFile /> */}
+    </div>
+  );
+};
+
+export default App;
